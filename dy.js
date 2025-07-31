ba26f4131fad3ce939dea66eec685d03598238c9
yy <111@qq.comqq.com>
2025-07-31 20:35:06 +0800

    7.31.1

--- dy.js
+++ dy.js
@@ -1,14 +1,14 @@
 import cheerio from "assets://js/lib/cheerio.min.js";
 import "assets://js/lib/crypto-js.js";
-import "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo1.js";
-import "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo2.js";
-import "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo4.js";
-import 模板 from "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo7.js";
+import "./lu228826/lu/refs/heads/main/mo1.js";
+import "./lu228826/lu/refs/heads/main/mo2.js";
+import "./lu228826/lu/refs/heads/main/mo4.js";
+import 模板 from ".//lu228826/lu/refs/heads/main/mo7.js";
 import {
     gbkTool
-} from "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo3.js";
-import "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo5.js";
-import "https://raw.githubusercontent.com/lu228826/lu/refs/heads/main/mo6.js";
+} from "./lu228826/lu/refs/heads/main/mo3.js";
+import "./lu228826/lu/refs/heads/main/mo5.js";
+import "./lu228826/lu/refs/heads/main/mo6.js";
 const _jinja2 = cheerio.jinja2;
 cheerio.jinja2 = function(template, obj) {
     try {
